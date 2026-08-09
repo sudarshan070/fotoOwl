c2f9748cd58c8543535a926d0a5cfe08e0972063
sudarshan-prima <sudarshan.shinde@primathon.in>
2026-08-09 10:12:54 +0530

    feat(media-core): finalize public package exports

--- packages/media-core/src/index.ts
+++ packages/media-core/src/index.ts
@@ -1,1 +1,14 @@
-export {};
+export { MediaClient } from './media-client';
+export { MediaEmitter } from './media-emitter';
+export type {
+  MediaConfig,
+  Photo,
+  Video,
+  MediaItem,
+  SearchParams,
+  CuratedParams,
+  PaginatedResponse,
+  MediaEventName,
+  MediaEventPayload
+} from './types';
+export { MediaApiError } from './types';
